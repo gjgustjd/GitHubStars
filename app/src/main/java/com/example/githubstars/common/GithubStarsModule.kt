63b33adbe1ec632d6f82dfdd59d4388f39dc41c0
gjgustjd <gjgustjd70@naver.com>
2022-04-17 04:44:30 +0900

    :recycle:Refactor:SearchUserResponse 변경
    Retrofit 데이터 수신 오류로 인한 SearchUserResponse.kt 파라미터 이름 변경
    GithubStarsModule에 누락된 InstallIn 추가

--- app/src/main/java/com/example/githubstars/common/GithubStarsModule.kt
+++ app/src/main/java/com/example/githubstars/common/GithubStarsModule.kt
@@ -5,16 +5,20 @@ import androidx.room.Room
 import com.example.githubstars.model.local.LocalUserDatabase
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class GithubStarsModule {
 
     @Provides
     @Singleton
-    fun getDatabase(@ApplicationContext context:Context):LocalUserDatabase
-    {
-        return Room.databaseBuilder(context,LocalUserDatabase::class.java,"user-db").build()
-    }
+    fun getDatabase(@ApplicationContext context: Context): LocalUserDatabase =
+        Room.databaseBuilder(context, LocalUserDatabase::class.java, "user-db")
+            .fallbackToDestructiveMigration().build()
 }
--- app/src/main/java/com/example/githubstars/model/dto/SearchUserResponse.kt
+++ app/src/main/java/com/example/githubstars/model/dto/SearchUserResponse.kt
@@ -2,6 +2,6 @@ package com.example.githubstars.model.dto
 
 data class SearchUserResponse(
     val incomplete_results: Boolean,
-    val userItems: List<UserItem>,
+    val items: List<UserItem>,
     val total_count: Int
 )
