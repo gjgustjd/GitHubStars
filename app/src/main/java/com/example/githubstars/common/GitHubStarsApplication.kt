b843d283c5d6991ba8b6b6abb826413afa950051
gjgustjd <gjgustjd70@naver.com>
2022-04-16 22:37:08 +0900

    :wrench:Chore:HiltApplication 적용
    GitHubStarsApplication.kt 추가 및 HiltApplication 적용

---
+++ app/src/main/java/com/example/githubstars/common/GitHubStarsApplication.kt
@@ -0,0 +1,8 @@
+package com.example.githubstars.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GitHubStarsApplication :Application(){
+}
