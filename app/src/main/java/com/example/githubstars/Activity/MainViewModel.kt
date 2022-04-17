3df78eb6831f0f42c7a9991a899c2667cbab3d04
gjgustjd <gjgustjd70@naver.com>
2022-04-17 16:19:26 +0900

    :recycle:Refactor:MainViewModel에 Hilt 적용
    MainViewModel에 Hilt 적용

--- app/src/main/java/com/example/githubstars/Activity/MainViewModel.kt
+++ app/src/main/java/com/example/githubstars/Activity/MainViewModel.kt
@@ -2,16 +2,25 @@ package com.example.githubstars.Activity
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.githubstars.model.GithubStarsRepository
 import com.example.githubstars.model.dto.UserItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
+    @Inject
+    lateinit var repository: GithubStarsRepository
     var userList = MutableLiveData<List<UserItem>>()
 
-    fun setupUserList() {
-        var response = GithubStarsRepository.getUsersList()
-        if (response.isSuccessful) {
-            userList.value = response.body()!!.userItems
+    fun setupUserList(word: String="") {
+        viewModelScope.launch {
+            var response = repository.getUsersList(search_string = word)
+            if (response.isSuccessful) {
+                userList.value = response.body()!!.items
+            }
         }
     }
 }
